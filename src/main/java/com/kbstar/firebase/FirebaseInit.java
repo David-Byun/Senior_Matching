53cef957e5bfe4786dc7a008a4879b0c452591ec
David Byun <byundavid@naver.com>
2023-06-02 15:46:36 +0900

    firebase 관련 push - David(0602)

---
+++ src/main/java/com/kbstar/firebase/FirebaseInit.java
@@ -0,0 +1,30 @@
+package com.kbstar.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+
+@Service
+public class FirebaseInit {
+    private static final String path = "nwitter-4c3ea-firebase-adminsdk-bko19-a9e441c4bb.json";
+
+    @PostConstruct
+    public void init(){
+        try{
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(new ClassPathResource(path).getInputStream())).build();
+
+            if(FirebaseApp.getApps().isEmpty()){
+                FirebaseApp.initializeApp(options);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+}
