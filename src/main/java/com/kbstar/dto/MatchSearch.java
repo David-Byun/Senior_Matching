58968bd0c1698328617ffc0e5e4a776fc1a84e14
abdevelo <124110316+abdevelo@users.noreply.github.com>
2023-06-02 14:57:15 +0900

    230602 매칭(결제완료까지)반영 - hspark
    230602 매칭(결제완료까지)반영 - hspark

---
+++ src/main/java/com/kbstar/dto/MatchSearch.java
@@ -0,0 +1,15 @@
+package com.kbstar.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class MatchSearch {
+    private String startDate;
+    private String endDate;
+}
