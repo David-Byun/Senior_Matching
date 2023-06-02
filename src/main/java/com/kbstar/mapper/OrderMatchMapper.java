fd10ab6d166f854452279086110f0cb9ebbd70d7
abdevelo <124110316+abdevelo@users.noreply.github.com>
2023-06-02 15:01:21 +0900

    230602 매칭(결제완료까지)반영 - hspark
    230602 매칭(결제완료까지)반영 - hspark

---
+++ src/main/java/com/kbstar/mapper/OrderMatchMapper.java
@@ -0,0 +1,16 @@
+package com.kbstar.mapper;
+
+import com.kbstar.dto.MatchSearch;
+import com.kbstar.dto.Mate;
+import com.kbstar.dto.OrderMatch;
+import com.kbstar.frame.ShopMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Mapper
+@Repository
+public interface OrderMatchMapper extends ShopMapper<Integer, OrderMatch> {
+    List<OrderMatch> findByMemberId(int memberId);
+    List<Mate> findByDate(MatchSearch ms);
+}
