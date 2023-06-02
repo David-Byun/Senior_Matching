b6ea24cdc337c30f783e448461ec4683d5b00a01
abdevelo <124110316+abdevelo@users.noreply.github.com>
2023-06-02 14:55:31 +0900

    230602 매칭(결제완료까지)반영 - hspark
    230602 매칭(결제완료까지)반영 - hspark

--- src/main/java/com/kbstar/dto/Match.java
+++ src/main/java/com/kbstar/dto/Match.java
@@ -1,19 +1,39 @@
 package com.kbstar.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Date;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Match {
     private int id;
+    private int memberId;
     private int mateId;
-    private Date startDate;
-    private Date endDate;
+    private String startDate;
+    private String endDate;
     private String service;
     private String area;
     private String objectGender;
     private String objectBirth;
-
     private String status;
     private int price;
     private String payment;
-    private Date payDate;
+    private String payDate;
+
+
+    public Match(OrderMatch om) {
+        this.memberId = om.getMemberId();
+        this.startDate = om.getStartDate();
+        this.endDate = om.getEndDate();
+        this.service = om.getService();
+        this.area = om.getService();
+        this.objectGender = om.getObjectGender();
+        this.objectBirth = om.getObjectBirth();
+    }
 }
