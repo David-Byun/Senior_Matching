54e32b2c2d1321a3d45afd4923016634683a8924
lydia-hr25 <134253919+lydia-hr25@users.noreply.github.com>
2023-06-13 09:56:28 +0900

    matereview추가
    matereview추가

---
+++ src/main/java/com/kbstar/dto/MateReviewSearch.java
@@ -0,0 +1,13 @@
+package com.kbstar.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class MateReviewSearch {
+    private int mateId;
+    private int rate;
+}
